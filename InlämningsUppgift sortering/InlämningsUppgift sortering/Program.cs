bc20cbc3d656ecd11858950cda2c4ecf13001d49
Elias-Olofsson <54439741+Elias-Olofsson@users.noreply.github.com>
2020-01-28 13:47:39 +0100

    Add files via upload

---
+++ InlämningsUppgift sortering/InlämningsUppgift sortering/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InlämningsUppgift_sortering
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] ickeSorteradArray = new int[10] { 2, 3, 8, 9, 2, 8, 2, 8, 0, 6 };
+			BubbelSort(ickeSorteradArray);
+			int plats = 0;
+			while (plats < ickeSorteradArray.Length)
+			{
+				Console.WriteLine(ickeSorteradArray[plats]);
+				plats = plats + 1;
+			}
+
+		}
+
+		static int[] BubbelSort(int[] array)
+		{
+			for (int i = 0; i < array.Length-1; i++)
+			{
+				for (int j = 0; j < array.Length - 1 - i; j++)
+				{
+					if (array[j] > array[j+1])
+					{
+						int temp = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = temp;
+					}
+				}
+			}
+			return array;
+		}
+
+		static int[] IntersectionSort(int[] array)
+		{
+			int i
+		}
+	}
+}
